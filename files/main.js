9f741b5343dfbad65dec224c30a27140a1cc8653
Ahmed Eltohfa <eltohfa01@gmail.com>
2025-04-24 01:37:53 +0200

    fix routing 3

--- files/main.js
+++ files/main.js
@@ -621,9 +621,10 @@ loadUser();
 loadUsers();
 
 const authButtons = document.getElementById('auth-buttons');
-let sign, login;
-(window.location.pathname === '/' || window.location.pathname === '/index.html' || window.location.pathname === '/webAssignment/index.html' || window.location.pathname === '/webAssignment/') ? sign = '/files/pages/signup.html' : sign = 'signup.html';
-(window.location.pathname === '/' || window.location.pathname === '/index.html' || window.location.pathname === '/webAssignment/index.html' || window.location.pathname === '/webAssignment/') ? login = '/files/pages/login.html' : login = 'login.html';
+
+const basePath = `${location.origin}/`;
+const sign = basePath + 'files/pages/signup.html';
+const login = basePath + 'files/pages/login.html';
 if (user && authButtons) {
 	authButtons.innerHTML = `
             <button class="logout-btn" id="logoutBtn">Logout</button>
