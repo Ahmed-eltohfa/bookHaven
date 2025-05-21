7aa4aed55312cb1c3b61b2385737c38c15c2f0b2
Youssef Elhelw <youssefelhelwai@gmail.com>
2025-05-21 06:15:03 +0300

    listing all borrowed books in the profile page is done

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -13,15 +13,6 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os
 from pathlib import Path
 
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-# ... other settings ...
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
-
-
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
